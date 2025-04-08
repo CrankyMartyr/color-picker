2c4036adc1f2d223dc2da07d9fa0c381ebf119bc
Saad Shaikh <shaikhsaad497@gmail.com>
2025-04-08 14:15:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Allows listening on all URLs
+    port: 3000, // Optional: Specify the port
+  },
+});
